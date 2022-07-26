250eaed1a73c355ff44620849365893a7a576b56
scaphyxx <martin-goessel@web.de>
2022-07-26 16:30:35 +0200

    Aufgabe 4.2 und 4.4

---
+++ Martin/Niederschlagsmenge/src/com/scaphyxx/Month.java
@@ -0,0 +1,21 @@
+package com.scaphyxx;
+
+/** Month */
+public class Month {
+
+  String name;
+  int ns;
+
+  public Month(String name, int ns) {
+    this.name = name;
+    this.ns = ns;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getNs() {
+    return ns;
+  }
+}
---
+++ Martin/Quadratgleichung/src/com/scaphyxx/Main.java
@@ -0,0 +1,17 @@
+package com.scaphyxx;
+
+/** main */
+public class Main {
+
+  public static void main(String[] args) {
+    System.out.println(qGleichung(4));
+    System.out.println(qGleichung(2));
+    System.out.println(qGleichung((double) 2/3));
+  }
+
+  private static String qGleichung(double x) {
+    double quad = x * x;
+    double ergebnis = 3 * quad - 8 * x + 4;
+    return "x = " + x + ": Ergebnis = " + ergebnis;
+  }
+}
