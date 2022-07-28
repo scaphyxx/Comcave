7a1ee644e0d6729d575c07cc89e53f80003ee578
MezzoTheMix <109968583+MezzoTheMix@users.noreply.github.com>
2022-07-28 12:22:57 +0200

    Add files via upload

--- Norman/src/basis/verzweigung/Mehrfachauswahl.java
+++ Norman/src/basis/verzweigung/Mehrfachauswahl.java
@@ -5,6 +5,10 @@ public class Mehrfachauswahl {
 	public static void main(String[] args) {
 		
 		String ampelfarbe = "blau";
+		final String rot = "rot";    		// final setzt die Variable rot als eine Konstante
+	//				 rot ="dunkelrot"; 		//Funktionier nicht, weil final
+											// nur erlaubt ein einziges Mal den Wert zuweisen
+		
 		
 		switch(ampelfarbe.toLowerCase()) { //toLowerCase() kurzfristige Umwandlung in Kleinbuchstaben
 		
