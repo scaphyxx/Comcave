db66dd8ddcf3dd39c8d34546e30fd8482bb0d151
scaphyxx <martin-goessel@web.de>
2022-08-01 17:19:38 +0200

    update Slotmachine

--- Martin/Aufgabe0712/src/com/scaphyxx/Main.java
+++ Martin/Aufgabe0712/src/com/scaphyxx/Main.java
@@ -1,5 +1,6 @@
 package com.scaphyxx;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /** main */
@@ -7,42 +8,86 @@ public class Main {
 
   private static int coins = 10;
   private static int bet;
+
+  private static String choice;
   private static Scanner sc = new Scanner(System.in);
 
   public static void main(String[] args) {
     // Slotmachine
     boolean quit = false;
-    System.out.println("Welcome to Slutty - the nicest slotmachine in the world!");
-    printMenu();
+    System.out.println(
+        "- Welcome to Slutty - "
+            + "\nThe nicest Slutmachine in the World!"
+            + "\nDu startest mit 10 Coins");
+    printStartMenu();
     while (!quit) {
-      switch (sc.nextLine()) {
+      choice = sc.nextLine();
+      switch (choice) {
         case "":
+          if (bet != 0) {
+            System.out.println(printResult(roll(choice)));
+            sc.nextLine();
+            printGameMenu();
+            break;
+          } else {
+            System.out.println(printResult(roll()));
+            sc.nextLine();
+            printGameMenu();
+            break;
+          }
+        case "c":
           System.out.println(printResult(roll()));
           sc.nextLine();
-          printMenu();
+          printGameMenu();
           break;
         default:
           quit = true;
-          System.out.println("\nTschuesssss...");
+          if (10 - coins == 1) {
+            System.out.println("LOSER!!! DU HAST 1 COIN VERLOREN!!!");
+            System.exit(0);
+          } else if (coins < 10) {
+            System.out.println("LOSER!!! DU HAST " + (10 - coins) + " COINS VERLOREN!!!");
+            System.exit(0);
+          }
+          System.out.println("\nSuper! DU HAST " + coins + " GEWONNEN");
           System.exit(0);
       }
     }
   }
 
-  public static void printMenu() {
+  public static void printStartMenu() {
     System.out.println("\nDruecke...");
-    System.out.println("Enter - Einmal Rollen");
-    System.out.println("Alles andere - Exit");
+    System.out.println("Enter\t\t - Einmal rollen");
+    System.out.println("Alles andere\t - Exit");
+  }
+
+  public static void printGameMenu() {
+    System.out.println("\nDruecke...");
+    System.out.println("Enter\t\t - Noch Einmal mit dem gleichen Betrag rollen");
+    System.out.println("C\t\t - Mit einem neuen Betrag rollen");
+    System.out.println("Alles andere\t - Exit");
   }
 
   public static int[] roll() {
     System.out.println("Wie viel willst du setzen?");
     bet = sc.nextInt();
     int[] slots = new int[3];
-    if (coins < 1) {
-      return null;
-    } else if (bet > coins) {
-      return null;
+    if (bet > coins) {
+      return new int[] {-1, -1, 0};
+    } else {
+      coins -= bet;
+      System.out.println("\nAm Rollen...");
+      for (int i = 0; i < slots.length; i++) {
+        slots[i] = random();
+      }
+    }
+    return slots;
+  }
+
+  public static int[] roll(String choice) {
+    int[] slots = new int[3];
+    if (bet > coins) {
+      return new int[] {-1, -1, 0};
     } else {
       coins -= bet;
       System.out.println("\nAm Rollen...");
@@ -77,16 +122,16 @@ public class Main {
     for (int i = 0; slots != null && i < slots.length; i++) {
       result += slots[i] + " ";
     }
+    if (Arrays.toString(slots).equals(Arrays.toString((new int[] {-1, -1, 0})))) {
+      return "Du hast nicht genug Coins!";
+    }
     int matches = checkSlutty(slots);
     switch (matches) {
       case 0:
-      if (bet > coins) {
-        return "Du hast nicht genug Coins! Dein Guthaben betraegt " + coins + " Coins";
-      }
-      if (coins == 0) {
-        System.out.println(result += "\n\nGAME OVER");
-        System.exit(0);
-      }
+        if (coins == 0) {
+          System.out.println(result += "\n\nGAME OVER");
+          System.exit(0);
+        }
         result += "- leider verloren, Guthaben " + coins + " Coins";
         break;
       case 1:
