89865e42c73c243b42106dc11d9d8b42edbf9d65
barpe <lorbaer@posteo.de>
2022-08-02 14:52:58 +0200

    Adding cc.lars.java.Calculation.java

---
+++ Lars/eclipse-Workspace/cc.lars.java/src/cc/lars/java/calculator/Calculation.java
@@ -0,0 +1,19 @@
+package cc.lars.java.calculator;
+
+public class Calculation {
+	static int addition(int x, int y) {
+		return x + y;
+	}
+	
+	static double addition(double x, int y) {
+		return x + y;
+	}
+	
+	static double addition(int x, double y) {
+		return x + y;
+	}
+	
+	static double addition(double x, double y) {
+		return x + y;
+	}
+}
