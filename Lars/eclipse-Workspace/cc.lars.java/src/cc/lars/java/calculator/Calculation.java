263a25b49df0379618999adbdfc04c9bf212886f
barpe <lorbaer@posteo.de>
2022-08-02 15:45:46 +0200

    Adjust src/cc/lars/java/calculator/Calculation.java : Adding function divide()

--- Lars/eclipse-Workspace/cc.lars.java/src/cc/lars/java/calculator/Calculation.java
+++ Lars/eclipse-Workspace/cc.lars.java/src/cc/lars/java/calculator/Calculation.java
@@ -1,6 +1,7 @@
 package cc.lars.java.calculator;
 
 public class Calculation {
+
 // Addition
 	static int sum(int x, int y) {
 		return x + y;
@@ -20,36 +21,69 @@ public class Calculation {
 
 // Subtraction
 	static int substract(int x, int y) {
-	return x - y;
+		return x - y;
 	}
 	
 	static double substract(double x, int y) {
-	return x - y;
+		return x - y;
 	}
 	
 	static double substract(int x, double y) {
-	return x - y;
+		return x - y;
 	}
 	
 	static double substract(double x, double y) {
-	return x - y;
+		return x - y;
 	}
 
 // Multiplication
 	static int multiply(int x, int y) {
-	return x * y;
+		return x * y;
 	}
 	
 	static double multiply(double x, int y) {
-	return x * y;
+		return x * y;
 	}
 
 	static double multiply(int x, double y) {
-	return x * y;
+		return x * y;
 	}
 		
 	static double multiply(double x, double y) {
-	return x * y;
+		return x * y;
+	}
+	
+// Division
+	static int divide(int x, int y) throws ArithmeticException {
+		if(y != 0)
+			return x / y;
+		else {
+			throw new ArithmeticException("Error: Division by Zero");
+		}
+	}
+	
+	static double divide(double x, int y) throws ArithmeticException {
+		if(y != 0)
+			return x / y;
+		else {
+			throw new ArithmeticException("Error: Division by Zero");
+		}
+	}
+
+	static double divide(int x, double y) throws ArithmeticException {
+		if(y != 0)
+			return x / y;
+		else {
+			throw new ArithmeticException("Error: Division by Zero");
+		}
+	}
+		
+	static double divide(double x, double y) throws ArithmeticException {
+		if(y != 0)
+			return x / y;
+		else {
+			throw new ArithmeticException("Error: Division by Zero");
+		}
 	}
 	
 /** var args : 
